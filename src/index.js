97a8ee0b374e537de71e4e15c5437e1927ccb06c
Odai Gubran <95129948+odai-dev@users.noreply.github.com>
2025-10-24 23:25:35 +0300

    Added the main methods for initilazing the app, add new projects, add new todos, delete todos, etc....

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
+import { createNewToDo } from './modules/logic';
 import createProject from './modules/project';
 import createToDo from './modules/todo';
 import './styles.css';
+import { getProjects, initApp } from './modules/logic';
+
+initApp();
 
 
-const p = createProject({name: "Coding"});
-console.log(p);
 
-const t = createToDo({title: "Finish The Odin Project to-do-list project", dueDate: "Today"});
-console.log(t);
 
-p.addToDo(t);
-console.log(p);
 
